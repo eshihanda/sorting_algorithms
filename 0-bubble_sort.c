bc51d3c1f9fe792711806b703266f249aa2f3cac
eshihanda <eshihandamonn@gmail.com>
2024-01-26 14:49:38 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,31 +9,25 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
-    int swapped; /* flag to check if swapping occured*/
+	size_t i = 0;
+	int flag = 1, tmp;
 
-    if (!array || size < 2)
-    {
-        return;
-    }
-    for ( i = 0; i < size - 1; i++)
-    {
-        swapped = 0; /*reset the flag for each swap*/
-        
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                swapped = 1; /* a swap occurred*/
-                print_array(array, size);
-            }
-        }
-        if (swapped == 0) /*array is already sorted*/
-            break;
-    }
-
+	if (!array || size < 2)
+		return;
+	while (flag)
+	{
+		flag = 0;
+		for (i = 0; i < (size - 1) && i != (size - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		i = 0;
+	}
 }
